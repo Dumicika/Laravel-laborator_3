f220d9bafb1d401d6ca648617937270a61f7ac9c
Dumi <dumidumitrita05@gmail.com>
2024-02-23 09:27:21 +0200

    pagination

--- database/migrations/2024_02_06_083640_create_phones_table.php
+++ database/migrations/2024_02_06_083640_create_phones_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')
             ->on('users')
-            ->onDelete('cascade')
+            ->onDelete('cascade') 
             ->onUpdate('cascade');
             $table->string('phone');
             $table->timestamps();
